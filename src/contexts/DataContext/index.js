155ba7e47466acd615022957956b4b520092b1be
patel dhruvesh <dpatel@outlook.fr>
2025-04-01 15:02:21 +0200

    recctification du code

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -1,39 +1,55 @@
-import { createContext, useContext, useState, useEffect, useMemo } from "react";
 import PropTypes from "prop-types";
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useMemo,
+  useCallback
+} from "react";
 
-export const DataContext = createContext();
+const DataContext = createContext({});
+
+export const api = {
+  loadData: async () => {
+    const response = await fetch("/events.json");
+    return response.json();
+  },
+};
 
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
+  const getData = useCallback(async () => {
+    try {
       setLoading(true);
-      try {
-        const response = await fetch("/events.json");
-        if (!response.ok) throw new Error("Erreur de chargement des données");
-        const fetchedData = await response.json();
-        setData(fetchedData);
-      } catch (fetchError) {
-        setError(fetchError);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
+      setData(await api.loadData());
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
+  useEffect(() => {
+    if (data) return;
+    getData();
+  }, [data, getData]);
+  
   const last = useMemo(() => 
     Array.isArray(data?.events) && data.events.length > 0
       ? [...data.events].sort((a, b) => new Date(b.date) - new Date(a.date))[0]
       : null
   , [data]);
-  
 
-  const contextValue = useMemo(() => ({ data, error, loading, last }), [data, error, loading, last]);
+  const contextValue = useMemo(() => ({
+    data,
+    error,
+    loading,
+    last,
+  }), [data, error, loading, last]);
 
   return (
     <DataContext.Provider value={contextValue}>
@@ -42,8 +58,10 @@ export const DataProvider = ({ children }) => {
   );
 };
 
-export const useData = () => useContext(DataContext);
-
 DataProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
+export const useData = () => useContext(DataContext);
+
+export default DataContext;
