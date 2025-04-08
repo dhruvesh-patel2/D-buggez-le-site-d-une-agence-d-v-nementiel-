d05bb4c92ba221595bea36326f4c9eb471dbbe1c
patel dhruvesh <dpatel@outlook.fr>
2025-04-08 20:51:05 +0200

    Correction des bugs:

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -16,7 +16,7 @@ const Select = ({
   const [value, setValue] = useState();
   const [collapsed, setCollapsed] = useState(true);
   const changeValue = (newValue) => {
-    onChange(newValue); // Modifié pour passer la valeur
+    onChange(newValue); 
     setValue(newValue);
     setCollapsed(newValue);
   };
